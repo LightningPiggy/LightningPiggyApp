b8d77a13cdb700548bb72379f0cfd4b18c746726
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-07-08 15:05:01 +0200

    add test scripts

---
+++ scripts/get_links_wrong_wallet.sh
@@ -0,0 +1,1 @@
+curl -H "X-Api-Key: cf5f9c2f2ee04f65826344342c7357ff" "https://demo.lnpiggy.com/lnurlp/api/v1/links?all_wallets=false"
---
+++ scripts/get_wrong_wallet.sh
@@ -0,0 +1,1 @@
+curl -H "X-Api-Key: cf5f9c2f2ee04f65826344342c7357ff" https://demo.lnpiggy.com/api/v1/wallet
